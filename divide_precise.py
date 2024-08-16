1209ab876c832fca1de6eaf8834a6c2e314d088a
Abdul-Rahman Ibrahim <89408640+Abdul-Rahman-Ibrahim@users.noreply.github.com>
2024-08-17 02:16:06 +0300

    Update divide_precise.py

--- divide_precise.py
+++ divide_precise.py
@@ -1,35 +1,32 @@
-def divide(n1,n2,p):
-    res = ""
-    while True:
-        count = 0
-        while n1 < n2:
-            n1 *= 10
-            if len(res) == 0:
-                count += 1
-                res += "0."
-            else:
-                count += 1
-                if count > 1:
-                    res += str(0)
-        d = float(n1/n2)
-        i = str(d).index(".")
-        num_floor = int(str(d)[:i])
-        if count == 0:
-            res += str(num_floor)+"."
-        else:
-            res += str(num_floor)
-        n1 = n1 - num_floor * n2
+def divide(n1, n2, p):
+    if n2 == 0:
+        print("Error: Division by zero is not allowed.")
+        return
 
-        if len(res)-1 >= p or n1 == 0:
+    # Performing integer division to get the integer part
+    integer_part = int(n1 // n2)
+    remainder = n1 - integer_part * n2
+    res = str(integer_part) + "."
+
+    # Loop to get the decimal part with the required precision
+    for _ in range(p):
+        remainder *= 10
+        digit = int(remainder // n2)
+        res += str(digit)
+        remainder -= digit * n2
+
+        if remainder == 0:
             break
-    if len(res) < p:
-        z = p - len(res)
-        res += "0"*z
+
+    # Adding trailing zeros if necessary to match the required precision
+    if len(res.split(".")[1]) < p:
+        res += "0" * (p - len(res.split(".")[1]))
 
     print(res)
 
-n1 = float(input("Enter value of numerator "))
-n2 = float(input("Enter value of denominator "))
-p = int(input("Enter precision of division. Please this should be an integer "))
 
-divide(n1,n2,p)
+n1 = float(input("Enter value of numerator: "))
+n2 = float(input("Enter value of denominator: "))
+p = int(input("Enter precision of division. Please, this should be an integer: "))
+
+divide(n1, n2, p)
